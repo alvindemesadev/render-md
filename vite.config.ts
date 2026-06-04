24991e8653e69ac638ee4c0c9c5397f93bb40147
Conan Clyde <alvindemesa.dev@gmail.com>
2026-06-05 02:39:15 +0800

    fix: rename vite.config.js to vite.config.ts to resolve wrangler parse error and configure wrangler pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
